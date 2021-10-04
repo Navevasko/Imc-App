263153e70eaeed7c29c59fdce0eed8cecc1030f4
GUILHERME NAVEVAIKO MOREIRA <21193389@CFP127ALUNO.LOCAL>
2021-10-04 16:57:42 -0300

    View do Login concluído e Dashboard quase concluída

---
+++ app/src/main/java/com/example/imcapp/DashBoardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.imcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
+        supportActionBar!! .hide()
+    }
+}
