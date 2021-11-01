bfa91c2b13bf2e3b925fec2fe2e0000a39f3486c
GUILHERME NAVEVAIKO MOREIRA <21193389@CFP127ALUNO.LOCAL>
2021-11-01 14:47:15 -0300

    DatePicker finalizado

--- app/src/main/java/com/example/imcapp/NovoUsuarioActivity.kt
+++ app/src/main/java/com/example/imcapp/NovoUsuarioActivity.kt
@@ -1,12 +1,16 @@
 package com.example.imcapp
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import java.util.*
 
 class NovoUsuarioActivity : AppCompatActivity() {
 
@@ -21,6 +25,9 @@ class NovoUsuarioActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo_usuario)
+
         editNome = findViewById(R.id.edit_nome)
         editEmail = findViewById(R.id.edit_email)
         editSenha = findViewById(R.id.edit_senha)
@@ -30,8 +37,46 @@ class NovoUsuarioActivity : AppCompatActivity() {
         rdoMasc = findViewById(R.id.rdo_masc)
         rdoFem = findViewById(R.id.rdo_fem)
 
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_novo_usuario)
+        //Criar um calendário
+
+        // *** Obter a data atual (hoje)
+
+
+
+
+        // Colocar um Listener de click no editText Data de Nascimento para abrir o calendário (DatePicker)
+
+        editDataNascimento.setOnClickListener {
+            CriarDatePicker()
+        }
+
+    }
+
+    private fun CriarDatePicker() {
+
+        val calendario = Calendar.getInstance()
+        val dia = calendario.get(Calendar.DAY_OF_MONTH)
+        val mes = calendario.get(Calendar.MONTH)
+        val ano = calendario.get(Calendar.YEAR)
+
+        val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, _ano, _mes, _dia ->
+
+            var mesReal = _mes + 1
+
+            var diaString = "$_dia"
+            var mesString = "$mesReal"
+
+            if (mesReal < 10) {
+                mesString = "0$mesReal"
+            }
+            if (_dia < 10) {
+                diaString = "0$_dia"
+            }
+
+            editDataNascimento.setText("$diaString/$mesString/$_ano")
+        }, ano, mes, dia)
+
+        datePicker.show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
